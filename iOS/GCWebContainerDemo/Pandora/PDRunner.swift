fccca2f67d7bbcd969ec24c1b56223fc79047d79
Gill Chan <jiahao0408@gmail.com>
2021-12-25 20:53:02 +0800

    feat: PDRunner

--- iOS/GCWebContainerDemo/Pandora/PDRunner.swift
+++ iOS/GCWebContainerDemo/Pandora/PDRunner.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import WebKit
+
+class PDRunner {
+    let pandora: Pandora
+    private var bgRunner: GCWebView?
+    init(pandora: Pandora) {
+        self.pandora = pandora
+    }
+    
+    func run() {
+        if let backgroundScript = pandora.background {
+            runBackgroundScript(backgroundScript)
+        }
+    }
+    
+    func runBackgroundScript(_ script: String) {
+        let bgWebView = GCWebView()
+        bgWebView.pd_addChromeBridge()
+        let userScript = WKUserScript(source: script,
+                                      injectionTime: .atDocumentEnd,
+                                      forMainFrameOnly: true)
+        bgWebView.addUserScript(userScript: userScript)
+        bgRunner = bgWebView
+        bgRunner?.loadHTMLString("<html></html>", baseURL: nil)
+    }
+    
+    func runContentScript(_ script: String) {
+        
+    }
+    
+    func runPageAction() {
+        
+    }
+    
+    func runBrowserAction() {
+        
+    }
+}
+
+private extension GCWebView {
+    func pd_addChromeBridge() {
+        if let path  = Bundle.main.path(forResource: "chrome", ofType: "js"),
+           let chrome = try? String(contentsOfFile: path) {
+            let userScript = WKUserScript(source: chrome,
+                                          injectionTime: .atDocumentStart,
+                                          forMainFrameOnly: true)
+            configuration.userContentController.addUserScript(userScript)
+        }
+    }
+}
