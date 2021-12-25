31fc6c1d7c90dae8d6a82187ee0b534a3fd07467
Gill Chan <jiahao0408@gmail.com>
2021-12-25 16:04:49 +0800

    feat: 支持内置 JS Service

--- web/src/service_loader.js
+++ web/src/service_loader.js
@@ -1,16 +1,17 @@
-import ServiceWrapper from './wrap_service';
+import ServiceWrapper, { wrapFunction } from './wrap_service';
 
 export default function loadAllServices() {
   const { services } = window.gc._config;
   const { global } = window.gc._config;
   const host = window[global].bridge;
   parseService(services, '', host);
+  loadDefaultServices();
 
   function parseService(service, namespace, host) {
     if (Array.isArray(service)) {
       return service
     }
-    Object.keys(service).map(name => {
+    Object.keys(service).forEach(name => {
       const newNamespace = namespace.length == 0 ? name : `${namespace}.${name}`;
       host[name] = Object()
       const methods = parseService(service[name], newNamespace, host[name])
@@ -31,4 +32,35 @@ export default function loadAllServices() {
 
 export function bridgeName(service) {
   return service[0].toLowerCase() + service.substring(1).split('Service')[0];
+}
+
+export function loadDefaultServices() {
+  var req = require.context("./services", true, /\.js$/);
+  const { global } = window.gc._config;
+  req.keys().forEach(function (key) {
+    const js = req(key);
+    console.log(js);
+    if (js.__esModule) {
+      const namespace = js.default.namespace;
+      let point = window[global].bridge;
+      namespace.split('.').forEach(name => {
+        if (!point[name]) {
+          point[name] = Object()
+        } else {
+          point = point[name]
+        }
+      })
+      const methods = Object.getOwnPropertyNames(js.default.prototype);
+      methods.forEach(m => {
+        if (m !== 'constructor') {
+          point[m] = function (arg) {
+            // todo: service cache
+            const service = (new js.default())
+            const fn = service[m]
+            return fn(arg)
+          }
+        }
+      })
+    }
+  });
 }
--- web/src/wrap_service.js
+++ web/src/wrap_service.js
@@ -1,6 +1,38 @@
 import invoker from "./invoker";
-import { bridgeName } from './service_loader'
+// import { bridgeName } from './service_loader'
 import Lock from "./lock";
+
+export function wrapFunction(name, bridge, method) {
+  let callbackFunc = function () {
+    const max = 9999;
+    const min = 0;
+    const random = parseInt(Math.random() * (max - min + 1) + min, 10);
+    const { global } = window.gc._config;
+    return `${global}_${name}_callback_func_${random}`;
+  }
+  function _initCallback(callback) {
+    window[callback] = function () {
+      const { global } = window.gc._config;
+      // 1. unlock
+      // 2. send msg to wrapFunction on service
+      lock.unlock(arguments[0])
+      // 3. remove callback
+      delete window[callback];
+    }
+  }
+  let lock = new Lock();
+  return async function () {
+    const callback = callbackFunc();
+    _initCallback(callback);
+    const params = JSON.parse(JSON.stringify(arguments));
+    // callback, need to be a function name but not a obj in window
+    lock = new Lock();
+    lock.lock();
+    await invoker(`${bridge}.${method}`, params, callback);
+    return await lock.status
+  }
+}
+
 export default class ServiceWrapper {
   constructor(serviceInfo) {
     this.serviceInfo = serviceInfo;
@@ -12,45 +44,9 @@ export default class ServiceWrapper {
     const { methods } = this.serviceInfo;
     methods.forEach(m => {
       if (m !== constructorFunc) {
-        const wrapFunction = async function () {
-          const callback = this.callback;
-          this._initCallback(callback);
-          const params = JSON.parse(JSON.stringify(arguments));
-          // callback, need to be a function name but not a obj in window
-          this.lock = new Lock();
-          this.lock.lock();
-          await invoker(`${this.serviceInfo.bridge}.${m}`, params, callback);
-          return await this.lock.status
-        }
-        this[m] = wrapFunction
+        const { name, bridge } = this.serviceInfo;
+        this[m] = wrapFunction(name, bridge, m)
       }
     });
-  }
-
-  _initCallback(callback) {
-    const name = bridgeName(this.serviceInfo.name);
-    const { bridge } = this.serviceInfo;
-    const wrapFunction = function () {
-      const { global } = window.gc._config;
-      let self = window[global].bridge;
-      bridge.split('.').forEach(e => {
-        self = self[e]
-      });
-      // 1. unlock
-      // 2. send msg to wrapFunction on service
-      self.lock.unlock(arguments[0])
-      // 3. remove callback
-      delete window[callback];
-    }
-    window[callback] = wrapFunction
-  }
-
-  get callback() {
-    const { name } = this.serviceInfo;
-    const max = 9999;
-    const min = 0;
-    const random = parseInt(Math.random() * (max - min + 1) + min, 10);
-    const { global } = window.gc._config;
-    return `${global}_${name}_callback_func_${random}`;
   }
 }
